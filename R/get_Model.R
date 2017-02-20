f5adea215c4a7a8f8e81d074a9589193c35bceab
romanel <a.romanel@gmail.com>
2017-02-20 11:24:06 +0100

    Examples data

--- R/get_Model.R
+++ R/get_Model.R
@@ -7,7 +7,7 @@ get.Model <- function(model.available,model.folder)
   }
   if(!file.exists(file.path(model.folder,paste(model.available,".Model.gds",sep=""))))
   {
-    download.file(paste("https://demichelislab.unitn.it/lib/exe/fetch.php?media=",model.available,".Model.gds",sep=""),
+    download.file(paste("https://github.com/aromanel/EthSEQ_Data/raw/master/EthSEQ_Models/",model.available,".Model.gds",sep=""),
                   file.path(model.folder,paste(model.available,".Model.gds",sep="")))
   }
   return(file.path(model.folder,paste(model.available,".Model.gds",sep="")))
