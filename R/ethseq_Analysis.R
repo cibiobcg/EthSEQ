483f237f6ab507874d85ac45a130971474f71763
Romanel <alessandro.romanel@sci-delta.unitn.it>
2017-04-01 15:13:18 +0200

    clean-up

--- R/ethseq_Analysis.R
+++ R/ethseq_Analysis.R
@@ -40,7 +40,7 @@ ethseq.Analysis <- function(
 {
   
   ## Version
-  EthSEQ_version <- "1.0"
+  EthSEQ_version <- "2.0.0"
   message.Date(paste("Running EthSEQ v",EthSEQ_version,sep=""))
   message.Date(paste("Working directory: ",out.dir,sep=""))
   
