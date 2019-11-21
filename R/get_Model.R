e0c612a2099fabc6d5244104b5981bdbcceab241
romanel <alessandro.romanel@unitn.it>
2019-11-21 23:26:47 +0100

    bug fix related with SNPRelate package naming changes

--- R/get_Model.R
+++ R/get_Model.R
@@ -7,7 +7,7 @@ get.Model <- function(model.available,model.folder)
   }
   if(!file.exists(file.path(model.folder,paste(model.available,".Model.gds",sep=""))))
   {
-    download.file(paste("https://github.com/aromanel/EthSEQ_Data/raw/master/EthSEQ_Models/",model.available,".Model.gds",sep=""),
+    download.file(paste("https://github.com/cibiobcg/EthSEQ_Data/raw/master/EthSEQ_Models/",model.available,".Model.gds",sep=""),
                   file.path(model.folder,paste(model.available,".Model.gds",sep="")))
   }
   return(file.path(model.folder,paste(model.available,".Model.gds",sep="")))
