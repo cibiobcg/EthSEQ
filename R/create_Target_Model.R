d5b898c8d93cde3c37cd6a0eb984a9a824ab751c
ddalfovo <davide.dalfovo@gmail.com>
2022-02-03 14:06:14 +0100

    Adding cpp version in the VCF function

--- R/create_Target_Model.R
+++ R/create_Target_Model.R
@@ -5,10 +5,6 @@ create.Target.Model <-function(sample.names,genotype.dir,out.dir,cores,bam.chr.e
   files = files[which(files%in%paste(sample.names,".genotype.vcf",sep=""))]
   geno = fread(paste(genotype.dir,files[1],sep=""),sep="\t",header=T,data.table=FALSE,showProgress = F)
   
-  message.Date("Cpp")
-  print(geno[1:5,1:15])
-  geno = splitGenoDF(geno)
-  print(geno[1:5,1:15])
   res = mclapply(files,function(f)
   {
     geno = fread(paste(genotype.dir,f,sep=""),sep="\t",header=T,data.table=FALSE,showProgress=F)
@@ -64,55 +60,59 @@ create.Target.Model <-function(sample.names,genotype.dir,out.dir,cores,bam.chr.e
 create.Target.Model.From.VCF <- function(vcf.fn,out.dir,cores)
 {
   vcf = fread(vcf.fn,sep="\t",data.table=FALSE,showProgress=FALSE)
-  message.Date("Cpp from VCF")
-  vcf = splitGenoDF(vcf)
   
   ### Chromosomes without chr encoding
   vcf[,1] = gsub("chr","",as.character(vcf[,1]))
   
-  geno = t(vcf[,10:ncol(vcf)])
-  res = mclapply(1:ncol(geno),function(i)
-  {
-    tmp = geno[,i]
-    tmp[which(tmp=="./.")] = "3"
-    tmp[which(tmp=="0/1")] = "1"
-    tmp[which(tmp=="0/0")] = "0"
-    tmp[which(tmp=="1/1")] = "2"
-    return(as.numeric(tmp))
-  },mc.cores=cores)
-  
-  geno = matrix(as.numeric(unlist(res)),nrow=length(res[[1]]),byrow=FALSE)
-  
-  mafs = 1-apply(geno,2,function(x) (length(which(x==0))*2+length(which(x==1)))/(length(which(x!=3))*2))
-  idx = which(mafs>0.5)
-  for(i in idx)
-  {
-    tmp = geno[,i]
-    tmp[which(tmp==0)] = 4
-    tmp[which(tmp==2)] = 0
-    tmp[which(tmp==4)] = 2
-    geno[,i] = tmp
-  }
-  
+#  geno = t(vcf[,10:ncol(vcf)])
+#  res = mclapply(1:ncol(geno),function(i)
+#  {
+#    tmp = geno[,i]
+#    tmp[which(tmp=="./.")] = "3"
+#    tmp[which(tmp=="0/1")] = "1"
+#    tmp[which(tmp=="0/0")] = "0"
+#    tmp[which(tmp=="1/1")] = "2"
+#    return(as.numeric(tmp))
+#  },mc.cores=cores)
+#  
+#  geno = matrix(as.numeric(unlist(res)),nrow=length(res[[1]]),byrow=FALSE)
+#  
+#  mafs = 1-apply(geno,2,function(x) (length(which(x==0))*2+length(which(x==1)))/(length(which(x!=3))*2))
+#  idx = which(mafs>0.5)
+#  for(i in idx)
+#  {
+#    tmp = geno[,i]
+#    tmp[which(tmp==0)] = 4
+#    tmp[which(tmp==2)] = 0
+#    tmp[which(tmp==4)] = 2
+#    geno[,i] = tmp
+#  }
+#  
   snp.allele = rep("A/B",ncol(geno))
-  snp.allele[idx] = "B/A"
+#  snp.allele[idx] = "B/A"
+  convertGeno(vcf,snp.allele)
+  
+  vcf.info = vcf[,1:9]
+  sample.id = colnames(vcf)[10:ncol(vcf)]
+  vcf = as.matrix(vcf[,10:ncol(vcf)])
+  mode(vcf) = "integer"
   
   snpgdsCreateGeno(file.path(out.dir,"Target.gds"),
-                   genmat = geno,
-                   sample.id = colnames(vcf)[10:ncol(vcf)],
+                   genmat = vcf,
+                   sample.id = sample.id,
                    snp.id = 1:ncol(geno),
-                   snp.rs.id = vcf[,3],
-                   snp.chromosome = vcf[,1],
-                   snp.position = vcf[,2],
+                   snp.rs.id = vcf.info[,3],
+                   snp.chromosome = vcf.info[,1],
+                   snp.position = vcf.info[,2],
                    snp.allele = snp.allele,
-                   snpfirstdim=FALSE)
+                   snpfirstdim=TRUE)
   
   genofile <- snpgdsOpen(file.path(out.dir,"Target.gds"),readonly = F)
   sample.annot <- data.frame(pop.group=rep("ND",nrow(geno)),sex=rep("M",nrow(geno)))
   add.gdsn(genofile,"sample.annot",sample.annot)
   
-  add.gdsn(genofile,"snp.ref",vcf[,4])
-  add.gdsn(genofile,"snp.alt",vcf[,5])
+  add.gdsn(genofile,"snp.ref",vcf.info[,4])
+  add.gdsn(genofile,"snp.alt",vcf.info[,5])
   
   snpgdsClose(genofile)
 }
