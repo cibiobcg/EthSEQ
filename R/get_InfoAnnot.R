a136d3c7f29bbfdb9ef7031bfbf9121c402fe40d
CibioBCG <you@example.com>
2022-09-14 11:50:51 +0200

    dev

--- R/get_InfoAnnot.R
+++ R/get_InfoAnnot.R
@@ -10,7 +10,6 @@ getModelsList <- function()
   exKit = system.file("extdata", "exonic_kits_map.tsv",
                       package="EthSEQ")
   df = fread(exKit,data.table = F)
-  # df = df[sapply(strsplit(df$assembly,","),function(obj){any(obj==assembly)})&sapply(strsplit(df$pop,","),function(obj){any(obj==pop)}),]
   return(df)
 }
 
